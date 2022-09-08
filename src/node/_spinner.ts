8bb80a1ad24a0579e57baac96338d5024f047cb8
Marius Lundgård <studio@mariuslundgard.com>
2022-09-08 22:06:44 +0200

    fix: simplify spinner

--- src/node/_spinner.ts
+++ src/node/_spinner.ts
@@ -1,26 +1,18 @@
+/* eslint-disable no-console */
+
 import chalk from 'chalk'
-import ora from 'ora'
 
 export function _spinner(msg: string): {complete: () => void; error: () => void} {
   const startTime = Date.now()
 
-  const spinner = ora({
-    spinner: 'dots',
-    text: `${chalk.yellow('run ')} ${msg}`,
-  })
-
-  spinner.color = 'yellow'
-
-  spinner.start()
+  console.log(`${chalk.yellow('run   ')} ${msg}`)
 
   return {
     complete: () => {
-      spinner.color = 'green'
-      spinner.succeed(`${chalk.green('ok  ')} ${msg} ${chalk.gray(`${Date.now() - startTime}ms`)}`)
+      console.log(`${chalk.green('ok    ')} ${chalk.gray(`${Date.now() - startTime}ms`)}`)
     },
     error: () => {
-      spinner.color = 'red'
-      spinner.fail(chalk.red(`${msg} failed ${chalk.gray(`${Date.now() - startTime}ms`)}`))
+      console.log(`${chalk.red(`failed`)} ${chalk.gray(`${Date.now() - startTime}ms`)}`)
     },
   }
 }
