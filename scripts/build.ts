df3c2bff6d01877051eb7e7b90b535c9bb5ea095
Marius Lundgård <studio@mariuslundgard.com>
2022-11-04 13:25:05 +0100

    build: use `--strict` flag

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,7 +1,7 @@
 import path from 'path'
 import {build} from '../src/node'
 
-build({cwd: path.resolve(__dirname, '..')}).catch((err) => {
+build({cwd: path.resolve(__dirname, '..'), strict: true}).catch((err) => {
   // eslint-disable-next-line no-console
   console.error(err.stack)
   process.exit(1)
