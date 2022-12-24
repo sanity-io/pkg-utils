d44f59d553ff325332daa31eb35b59d45eae6632
Cody Olsen <stipsan@gmail.com>
2022-12-24 02:08:18 +0100

    fix: remove `@babel/plugin-proposal-object-rest-spread`

--- src/node/tasks/rollup/resolveRollupConfig.ts
+++ src/node/tasks/rollup/resolveRollupConfig.ts
@@ -100,7 +100,6 @@ export function resolveRollupConfig(
     }),
     getBabelOutputPlugin({
       babelrc: false,
-      plugins: ['@babel/plugin-proposal-object-rest-spread'],
       presets: [
         [
           '@babel/preset-env',
