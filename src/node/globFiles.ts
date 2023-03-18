990af9c0f26266b1dc7a0f1e2b8e096a95f01388
Vitor Delarco <vitorrd@outlook.com>
2023-03-18 14:41:35 +0100

    fix: Globs not working on Windows.

--- src/node/globFiles.ts
+++ src/node/globFiles.ts
@@ -1,5 +1,6 @@
 import globby from 'globby'
+import path from 'path'
 
 export function globFiles(patterns: string[]): Promise<string[]> {
-  return globby(patterns)
+  return globby(patterns.map(pattern => pattern.split(path.sep).join(path.posix.sep)))
 }
