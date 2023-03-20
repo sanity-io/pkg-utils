e8e3703610ddffbad7a9adfc16864731850f255b
Cody Olsen <stipsan@gmail.com>
2023-03-20 11:19:15 +0100

    chore: apply code review fixes

--- src/node/globFiles.ts
+++ src/node/globFiles.ts
@@ -1,6 +1,6 @@
-import globby from 'globby'
 import path from 'path'
+import globby from 'globby'
 
 export function globFiles(patterns: string[]): Promise<string[]> {
-  return globby(patterns.map(pattern => pattern.split(path.sep).join(path.posix.sep)))
+  return globby(patterns.map((pattern) => pattern.split(path.sep).join(path.posix.sep)))
 }
