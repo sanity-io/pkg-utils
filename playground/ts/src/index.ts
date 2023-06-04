d0255780336793adef0d7b49e93b21618c154914
Marius Lundgård <studio@mariuslundgard.com>
2023-06-04 08:45:38 +0200

    chore: format comments

--- playground/ts/src/index.ts
+++ playground/ts/src/index.ts
@@ -2,5 +2,5 @@
 export const VERSION = '1.0.0'
 export * from './module1'
 
-//if this line is uncommented, a test should fail
-//export * from './not-imported'
+// if this line is uncommented, a test should fail
+// export * from './not-imported'
--- playground/ts/src/not-imported.ts
+++ playground/ts/src/not-imported.ts
@@ -1,8 +1,8 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-//@ts-expect-error unused
+// @ts-expect-error unused
 import {IncludedModuleDummy} from './module2'
 
-//this should not appear in our final bundle
+// this should not appear in our final bundle
 declare module './module2' {
   interface Excluded {
     field: string
