62070328fa3bba3183b7ea72c485c91d78ed457b
Cody Olsen <stipsan@gmail.com>
2023-08-06 01:54:12 +0200

    chore: test nextjs SSR behavior with conditionals

--- playground/runtime-next-js/pages/index.tsx
+++ playground/runtime-next-js/pages/index.tsx
@@ -1,7 +1,24 @@
-import * as index from 'dummy-module'
-import * as extra from 'dummy-module/extra'
+import type { InferGetStaticPropsType, GetStaticProps } from 'next'
+import {useState, useEffect} from 'react'
 
-export default function IndexPage() {
+import * as _index from 'dummy-module'
+import * as _extra from 'dummy-module/extra'
+
+type Props = {
+  index: typeof _index
+  extra: typeof _extra
+}
+
+export const getStaticProps: GetStaticProps<Props> =  () => {
+  return { props: JSON.parse(JSON.stringify({ index: _index, extra: _extra })) }
+}
+
+export default function IndexPage(props: InferGetStaticPropsType<typeof getStaticProps>) {
+  // Because we use conditional exports, the values of `index` and `extra` are
+  // different on the server and client. This is a problem for React, because
+  // it expects the values to be the same on the server and client.
+  const [{index, extra}, setState] = useState(props)
+  useEffect(() => setState({ index: _index, extra: _extra }), [])
   return (
     <div>
       <div>
