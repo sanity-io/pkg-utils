646559955b1c0d5379c4ac99ff7731b6763ce6df
Cody Olsen <81981+stipsan@users.noreply.github.com>
2023-08-06 01:48:40 +0200

    chore: add vitest-github-actions-reporter (#169)

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,4 +1,5 @@
 import {defineConfig} from 'vitest/config'
+import GithubActionsReporter from 'vitest-github-actions-reporter'
 
 export default defineConfig({
   test: {
@@ -6,6 +7,9 @@ export default defineConfig({
 
     // Set to 2 minutes to support long-running Next.js test
     testTimeout: 2 * 60 * 1000,
+
+    // Enable rich PR failed test annotation on the CI
+    reporters: process.env.GITHUB_ACTIONS ? ['default', new GithubActionsReporter()] : 'default',
   },
   esbuild: {
     target: 'node14',
