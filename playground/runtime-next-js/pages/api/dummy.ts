d8c72cc89445696614e8cb19e6f57f5f9bb76609
Cody Olsen <81981+stipsan@users.noreply.github.com>
2023-08-06 00:49:05 +0200

    chore: test nextjs api handler bundling

---
+++ playground/runtime-next-js/pages/api/dummy.ts
@@ -0,0 +1,11 @@
+import * as index from 'dummy-module'
+import * as extra from 'dummy-module/extra'
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  res.status(200).json({ index, extra })
+}
