b9581d269eefb3118da9b8b3e17b16147f6f1e14
Cody Olsen <81981+stipsan@users.noreply.github.com>
2023-08-06 01:01:31 +0200

    chore: linter rants again

--- playground/runtime-next-js/pages/api/dummy.ts
+++ playground/runtime-next-js/pages/api/dummy.ts
@@ -1,6 +1,6 @@
 import * as index from 'dummy-module'
 import * as extra from 'dummy-module/extra'
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type {  NextApiResponse } from 'next'
 
 
 export default function handler(
