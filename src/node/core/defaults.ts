5ee90d8eda5538a41fc3aae6d45ca24eb68e4761
Cody Olsen <stipsan@gmail.com>
2024-01-25 18:30:19 +0100

    feat: add support for shared browerslist configs

--- src/node/core/defaults.ts
+++ src/node/core/defaults.ts
@@ -1,7 +1,4 @@
+import config from '@sanity/browserslist-config'
+
 /** @public */
-export const DEFAULT_BROWSERSLIST_QUERY = [
-  '> 0.2% and supports es6-module and supports es6-module-dynamic-import',
-  'not dead',
-  'not IE 11',
-  'maintained node versions',
-]
+export const DEFAULT_BROWSERSLIST_QUERY = config
