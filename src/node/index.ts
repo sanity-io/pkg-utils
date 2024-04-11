6b9e7a0a561c49bb09816ce34a3f9247b51f78a0
Cody Olsen <81981+stipsan@users.noreply.github.com>
2024-04-11 23:00:25 +0200

    fix: add `parseStrictOptions` export (#734)

--- src/node/index.ts
+++ src/node/index.ts
@@ -4,6 +4,12 @@ export * from './core'
 export * from './init'
 export * from './logger'
 export * from './resolveBuildTasks'
-export {type InferredStrictOptions, strictOptions, toggle, type ToggleType} from './strict'
+export {
+  type InferredStrictOptions,
+  parseStrictOptions,
+  strictOptions,
+  toggle,
+  type ToggleType,
+} from './strict'
 export * from './tasks'
 export * from './watch'
