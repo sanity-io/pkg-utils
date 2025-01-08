sanity-io/pkg-utils

commit af37de2abd614017e6f1c9ba2bba7490ebee03eb

fix(deps): Update dependency @microsoft/api-extractor to v7.49.0 (#1298)
Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>

pnpm-lock.yaml
@@ -21,8 +21,8 @@ importers:
         specifier: ^7.26.3
         version: 7.26.3
       '@microsoft/api-extractor':
-        specifier: 7.48.1
-        version: 7.48.1(@types/node@20.17.10)
+        specifier: 7.49.0
+        version: 7.49.0(@types/node@20.17.10)
       '@microsoft/tsdoc-config':
         specifier: 0.17.1
         version: 0.17.1
@@ -240,9 +240,6 @@ importers:
       vitest:
         specifier: ^2.1.8
         version: 2.1.8(@types/node@20.17.10)(terser@5.37.0)
-      vitest-github-actions-reporter:
-        specifier: ^0.11.1
-        version: 0.11.1(vitest@2.1.8(@types/node@20.17.10)(terser@5.37.0))
 
   playground/babel-plugin:
     devDependencies:
@@ -543,18 +540,6 @@ importers:
         version: link:../sanity
 
 packages:
-
-  '@actions/core@1.11.1':
-    resolution: {integrity: sha512-hXJCSrkwfA46Vd9Z3q4cpEpHB1rL5NG04+/rbqW9d3+CSvtB1tYe8UTpAlixa1vj0m/ULglfEK2UKxMGxCxv5A==}
-
-  '@actions/exec@1.1.1':
-    resolution: {integrity: sha512-+sCcHHbVdk93a0XT19ECtO/gIXoxvdsgQLzb2fE2/5sIZmWQuluYyjPQtrtTHdU1YzTZ7bAPN4sITq2xi1679w==}
-
-  '@actions/http-client@2.2.3':
-    resolution: {integrity: sha512-mx8hyJi/hjFvbPokCg4uRd4ZX78t+YyRPtnKWwIl+RzNaVuFpQHfmlGVfsKEJN8LwTCvL+DfVgAM04XaHkm6bA==}
-
-  '@actions/io@1.1.3':
-    resolution: {integrity: sha512-wi9JjgKLYS7U/z8PPbco+PvTb/nRWjeoFlJ1Qer83k/3C5PHQi28hiVdeE2kHXmIL99mQFawx8qt/JPjZilJ8Q==}
 
   '@ampproject/remapping@2.3.0':
     resolution: {integrity: sha512-30iZtAPgz+LTIYoeivqYo853f02jBYSd5uGnGpkFV0M3xOt9aN73erkgYAmZU43x4VfqcnLxW9Kpg3R5LC4YYw==}
@@ -1129,10 +1114,6 @@ packages:
     resolution: {integrity: sha512-zSkKow6H5Kdm0ZUQUB2kV5JIXqoG0+uH5YADhaEHswm664N9Db8dXSi0nMJpacpMf+MyyglF1vnZohpEg5yUtg==}
     engines: {node: ^18.18.0 || ^20.9.0 || >=21.1.0}
 
-  '@fastify/busboy@2.1.1':
-    resolution: {integrity: sha512-vBZP4NlzfOlerQTnba4aqZoMhE/a9HY7HRqoOPaETQcSQuWEIyZMHGfVu6w9wGtGK5fED5qRs2DteVCjOH60sA==}
-    engines: {node: '>=14'}
-
   '@fontsource/fira-mono@5.1.1':
     resolution: {integrity: sha512-JDZUQuQQFTvPJgwOftXg44LOa7XYNqwTHHquEpi8rY6SK8m7Q5rWxmMX7nUOvRF2g+jc02mqmK2Y+WmviF9+VQ==}
 
@@ -1297,8 +1278,8 @@ packages:
   '@microsoft/api-extractor-model@7.30.1':
     resolution: {integrity: sha512-CTS2PlASJHxVY8hqHORVb1HdECWOEMcMnM6/kDkPr0RZapAFSIHhg9D4jxuE8g+OWYHtPc10LCpmde5pylTRlA==}
 
-  '@microsoft/api-extractor@7.48.1':
-    resolution: {integrity: sha512-HN9Osa1WxqLM66RaqB5nPAadx+nTIQmY/XtkFdaJvusjG8Tus++QqZtD7KPZDSkhEMGHsYeSyeU8qUzCDUXPjg==}
+  '@microsoft/api-extractor@7.49.0':
+    resolution: {integrity: sha512-X5b462k0/yl8qWdGx3siq5vyI8fTDU9fRnwqTMlGHqFhLxpASmLWA2EU6nft+ZG8cQM2HRZlr4HSo62UqiAnug==}
     hasBin: true
 
   '@microsoft/tsdoc-config@0.17.1':
@@ -5985,10 +5966,6 @@ packages:
   tunnel-agent@0.6.0:
     resolution: {integrity: sha512-McnNiV1l8RYeY8tBgEpuodCC1mLUdbSN+CYBL7kJsJNInOP8UjDDEwdk6Mw60vdLLrr5NHKZhMAOSrR2NZuQ+w==}
 
-  tunnel@0.0.6:
-    resolution: {integrity: sha512-1h/Lnq9yajKY2PEbBadPXj3VxsDDu844OnaAo52UVmIzIvwwtBPIuNvkjuzBlTWpfJyUbG3ez0KSBibQkj4ojg==}
-    engines: {node: '>=0.6.11 <=0.7.0 || >=0.7.3'}
-
   type-check@0.4.0:
     resolution: {integrity: sha512-XleUoc9uwGXqjWwXaUTZAmzMcFZ5858QA2vvx1Ur5xIcixXIP+8LnFDgRplU30us6teqdlskFfu+ae4K79Ooew==}
     engines: {node: '>= 0.8.0'}
@@ -6024,11 +6001,6 @@ packages:
     resolution: {integrity: sha512-3KS2b+kL7fsuk/eJZ7EQdnEmQoaho/r6KUef7hxvltNA5DR8NAUM+8wJMbJyZ4G9/7i3v5zPBIMN5aybAh2/Jg==}
     engines: {node: '>= 0.4'}
 
-  typescript@5.4.2:
-    resolution: {integrity: sha512-+2/g0Fds1ERlP6JsakQQDXjZdZMM+rqpamFZJEKh4kwTIn3iDkgKtby0CeNd5ATNZ4Ry1ax15TMx0W2V+miizQ==}
-    engines: {node: '>=14.17'}
-    hasBin: true
-
   typescript@5.7.2:
     resolution: {integrity: sha512-i5t66RHxDvVN40HfDd1PsEThGNnlMCMT3jMUuoh9/0TaqWevNontacunWyN02LA9/fIbEWlcHZcgTKb9QoaLfg==}
     engines: {node: '>=14.17'}
@@ -6063,10 +6035,6 @@ packages:
 
   undici-types@6.19.8:
     resolution: {integrity: sha512-ve2KP6f/JnbPBFyobGHuerC9g1FYGn/F8n1LWTwNxCEzd6IfqTwUQcNXgEtmmQ6DlRrC1hrSrBnCZPokRrDHjw==}
-
-  undici@5.28.4:
-    resolution: {integrity: sha512-72RFADWFqKmUb2hmmvNODKL3p9hcB6Gt2DOQMis1SEBaV6a4MH8soBvzg+95CYhCKPFedut2JY9bMfrDl9D23g==}
-    engines: {node: '>=14.0'}
 
   unenv@1.10.0:
     resolution: {integrity: sha512-wY5bskBQFL9n3Eca5XnhH6KbUo/tfvkwm9OpcdCvLaeA7piBNbavbOKJySEwQ1V0RH6HvNlSAFRTpvTqgKRQXQ==}
@@ -6341,12 +6309,6 @@ packages:
       vite:
         optional: true
 
-  vitest-github-actions-reporter@0.11.1:
-    resolution: {integrity: sha512-ZHHB0wBgOPhMYCB17WKVlJZa+5SdudBZFoVoebwfq3ioIUTeLQGYHwh85vpdJAxRghLP8d0qI/6eCTueGyDVXA==}
-    engines: {node: '>=14.16.0'}
-    peerDependencies:
-      vitest: '>=0.28.5'
-
   vitest@2.1.8:
     resolution: {integrity: sha512-1vBKTZskHw/aosXqQUlVWWlGUxSJR8YtiyZDJAFeW2kPAeX6S3Sool0mjspO+kXLuxVWlEDDowBAeqeAQefqLQ==}
     engines: {node: ^18.0.0 || >=20.0.0}
@@ -6579,22 +6541,6 @@ packages:
     resolution: {integrity: sha512-bXE4cR/kVZhKZX/RjPEflHaKVhUVl85noU3v6b8apfQEc1x4A+zBxjZ4lN8LqGd6WZ3dl98pY4o717VFmoPp+A==}
 
 snapshots:
-
-  '@actions/core@1.11.1':
-    dependencies:
-      '@actions/exec': 1.1.1
-      '@actions/http-client': 2.2.3
-
-  '@actions/exec@1.1.1':
-    dependencies:
-      '@actions/io': 1.1.3
-
-  '@actions/http-client@2.2.3':
-    dependencies:
-      tunnel: 0.0.6
-      undici: 5.28.4
-
-  '@actions/io@1.1.3': {}
 
   '@ampproject/remapping@2.3.0':
     dependencies:
@@ -7174,8 +7120,6 @@ snapshots:
     dependencies:
       levn: 0.4.1
 
-  '@fastify/busboy@2.1.1': {}
-
   '@fontsource/fira-mono@5.1.1': {}
 
   '@humanfs/core@0.19.1': {}
@@ -7311,7 +7255,7 @@ snapshots:
     transitivePeerDependencies:
       - '@types/node'
 
-  '@microsoft/api-extractor@7.48.1(@types/node@20.17.10)':
+  '@microsoft/api-extractor@7.49.0(@types/node@20.17.10)':
     dependencies:
       '@microsoft/api-extractor-model': 7.30.1(@types/node@20.17.10)
       '@microsoft/tsdoc': 0.15.1
@@ -7325,7 +7269,7 @@ snapshots:
       resolve: 1.22.9
       semver: 7.5.4
       source-map: 0.6.1
-      typescript: 5.4.2
+      typescript: 5.7.2
     transitivePeerDependencies:
       - '@types/node'
 
@@ -12845,8 +12789,6 @@ snapshots:
     dependencies:
       safe-buffer: 5.2.1
 
-  tunnel@0.0.6: {}
-
   type-check@0.4.0:
     dependencies:
       prelude-ls: 1.2.1
@@ -12892,8 +12834,6 @@ snapshots:
       possible-typed-array-names: 1.0.0
       reflect.getprototypeof: 1.0.9
 
-  typescript@5.4.2: {}
-
   typescript@5.7.2: {}
 
   ufo@1.5.4: {}
@@ -12927,10 +12867,6 @@ snapshots:
   uncrypto@0.1.3: {}
 
   undici-types@6.19.8: {}
-
-  undici@5.28.4:
-    dependencies:
-      '@fastify/busboy': 2.1.1
 
   unenv@1.10.0:
     dependencies:
@@ -13136,11 +13072,6 @@ snapshots:
   vitefu@1.0.4(vite@6.0.7(@types/node@20.17.10)(jiti@2.4.2)(terser@5.37.0)(yaml@2.6.1)):
     optionalDependencies:
       vite: 6.0.7(@types/node@20.17.10)(jiti@2.4.2)(terser@5.37.0)(yaml@2.6.1)
-
-  vitest-github-actions-reporter@0.11.1(vitest@2.1.8(@types/node@20.17.10)(terser@5.37.0)):
-    dependencies:
-      '@actions/core': 1.11.1
-      vitest: 2.1.8(@types/node@20.17.10)(terser@5.37.0)
 
   vitest@2.1.8(@types/node@20.17.10)(terser@5.37.0):
     dependencies:

src/node/tasks/dts/createApiExtractorConfig.ts
@@ -26,44 +26,24 @@ export function createApiExtractorConfig(options: {
     tsconfigPath,
   } = options
 
-  // When the module is 'preserve' it's necessary to override the tsconfig file as api-extractor doesn't handle it out of the box
-  const workaroundModulePreserve = tsconfig.options.module === ts.ModuleKind.Preserve
-  // If `paths` are used for self-referencing imports (e.g. the module is named `sanity`, and the `sanity/structure` export is also importing from `sanity/router`),
-  const workaroundPaths = !!tsconfig.options.paths
-
-  const overrideTsconfig = {
-    extends: tsconfigPath,
-    compilerOptions: {},
-  }
-
-  if (workaroundModulePreserve) {
-    Object.assign(overrideTsconfig.compilerOptions, {
-      // Set the equivalent options to `module: 'Preserve'`
-      // https://github.com/microsoft/TypeScript/pull/56785/files?file-filters%5B%5D=.js&file-filters%5B%5D=.json&file-filters%5B%5D=.symbols&file-filters%5B%5D=.ts&file-filters%5B%5D=.types&show-viewed-files=true#diff-31d3c12bafea26bc9e8c8a77920c41af0c593206442add70c45a06c063767445
-      module: 'ESNext',
-      moduleResolution: 'Bundler',
-      esModuleInterop: true,
-      resolveJsonModule: true,
-    })
-  }
-
-  if (workaroundPaths) {
-    Object.assign(overrideTsconfig.compilerOptions, {
-      // An empty object replaces whatever is in the original tsconfig file
-      paths: {},
-    })
-  }
-
   return {
     apiReport: {
       enabled: false,
       reportFileName: '<unscopedPackageName>.api.md',
     },
     bundledPackages,
-    compiler:
-      workaroundModulePreserve || workaroundPaths
-        ? {overrideTsconfig}
-        : {tsconfigFilePath: tsconfigPath},
+    // If `paths` are used for self-referencing imports (e.g. the module is named `sanity`, and the `sanity/structure` export is also importing from `sanity/router`),
+    compiler: tsconfig.options.paths
+      ? {
+          overrideTsconfig: {
+            extends: tsconfigPath,
+            compilerOptions: {
+              // An empty object replaces whatever is in the original tsconfig file
+              paths: {},
+            },
+          },
+        }
+      : {tsconfigFilePath: tsconfigPath},
 
     docModel: {
       enabled: false,

test/__snapshots__/cli.test.ts.snap
@@ -183,8 +183,16 @@ export declare function definePlugin(plugin: Plugin_2): Plugin_2
 /** @public */
 declare interface Plugin_2 {
   name: string
+  options?: PluginOptions
 }
 export {Plugin_2 as Plugin}
+
+declare interface PluginOptions {
+  /**
+   * @defaultValue true
+   */
+  enabled?: boolean
+}
 
 export {}
 "
@@ -219,10 +227,21 @@ export {
 `;
 
 exports[`should build \`multi-export\` package 6`] = `
-"import {Plugin as Plugin_2} from 'multi-export'
+"/** @public */
+export declare function plugin(): Plugin_2
 
 /** @public */
-export declare function plugin(): Plugin_2
+declare interface Plugin_2 {
+  name: string
+  options?: PluginOptions
+}
+
+declare interface PluginOptions {
+  /**
+   * @defaultValue true
+   */
+  enabled?: boolean
+}
 
 export {}
 "

playground/multi-export/src/definePlugin.ts
@@ -1,4 +1,4 @@
-import {type Plugin} from './types'
+import type {Plugin} from './types'
 
 /** @public */
 export function definePlugin(plugin: Plugin): Plugin {

playground/multi-export/src/types.ts
@@ -1,6 +1,14 @@
+interface PluginOptions {
+  /**
+   * @defaultValue true
+   */
+  enabled?: boolean
+}
+
 /** @public */
 export interface Plugin {
   name: string
+  options?: PluginOptions
 }
 
 /** @public */

playground/runtime-next-js/next-env.d.ts
@@ -3,4 +3,4 @@
 /// <reference types="next/navigation-types/compat/navigation" />
 
 // NOTE: This file should not be edited
-// see https://nextjs.org/docs/app/building-your-application/configuring/typescript for more information.
+// see https://nextjs.org/docs/app/api-reference/config/typescript for more information.

playground/sanity/repo.package.config.ts
@@ -20,13 +20,11 @@ export default defineConfig({
     ],
     rules: {
       // Disable rules for now
-      'ae-forgotten-export': 'off',
       'ae-incompatible-release-tags': 'off',
       'ae-internal-missing-underscore': 'off',
       'ae-missing-release-tag': 'off',
     },
   },
-  legacyExports: true,
   rollup: {
     optimizeLodash: true,
   },

playground/sanity/router.js
@@ -1,2 +1,0 @@
-// AUTO-GENERATED – DO NOT EDIT
-export * from './lib/router.esm'

commit 445ffc9026cf6e090b76db7fdfd003bbc7afb72b

fix: revert regression in #1298

pnpm-lock.yaml
@@ -21,8 +21,8 @@ importers:
         specifier: ^7.26.3
         version: 7.26.3
       '@microsoft/api-extractor':
-        specifier: 7.49.0
-        version: 7.49.0(@types/node@20.17.10)
+        specifier: 7.48.1
+        version: 7.48.1(@types/node@20.17.10)
       '@microsoft/tsdoc-config':
         specifier: 0.17.1
         version: 0.17.1
@@ -332,8 +332,8 @@ importers:
         version: 19.0.0-beta-63e3235-20250105(react@18.3.1)
     devDependencies:
       '@types/react':
-        specifier: ^18.3.18
-        version: 18.3.18
+        specifier: ^19.0.4
+        version: 19.0.4
       babel-plugin-react-compiler:
         specifier: beta
         version: 19.0.0-beta-63e3235-20250105
@@ -344,8 +344,8 @@ importers:
   playground/react-19:
     devDependencies:
       '@types/react':
-        specifier: ^19.0.3
-        version: 19.0.3
+        specifier: ^19.0.4
+        version: 19.0.4
       babel-plugin-react-compiler:
         specifier: beta
         version: 19.0.0-beta-63e3235-20250105
@@ -401,8 +401,8 @@ importers:
   playground/runtime-next-js:
     devDependencies:
       '@types/react':
-        specifier: ^19.0.3
-        version: 19.0.3
+        specifier: ^19.0.4
+        version: 19.0.4
       dummy-module:
         specifier: workspace:*
         version: link:../dummy-module
@@ -515,8 +515,8 @@ importers:
   playground/sanity:
     devDependencies:
       '@types/react':
-        specifier: ^19.0.3
-        version: 19.0.3
+        specifier: ^19.0.4
+        version: 19.0.4
       babel-plugin-react-compiler:
         specifier: beta
         version: 19.0.0-beta-63e3235-20250105
@@ -542,8 +542,8 @@ importers:
   playground/use-client-directive:
     devDependencies:
       '@types/react':
-        specifier: ^19.0.3
-        version: 19.0.3
+        specifier: ^19.0.4
+        version: 19.0.4
       react:
         specifier: ^19.0.0
         version: 19.0.0
@@ -1293,8 +1293,8 @@ packages:
   '@microsoft/api-extractor-model@7.30.1':
     resolution: {integrity: sha512-CTS2PlASJHxVY8hqHORVb1HdECWOEMcMnM6/kDkPr0RZapAFSIHhg9D4jxuE8g+OWYHtPc10LCpmde5pylTRlA==}
 
-  '@microsoft/api-extractor@7.49.0':
-    resolution: {integrity: sha512-X5b462k0/yl8qWdGx3siq5vyI8fTDU9fRnwqTMlGHqFhLxpASmLWA2EU6nft+ZG8cQM2HRZlr4HSo62UqiAnug==}
+  '@microsoft/api-extractor@7.48.1':
+    resolution: {integrity: sha512-HN9Osa1WxqLM66RaqB5nPAadx+nTIQmY/XtkFdaJvusjG8Tus++QqZtD7KPZDSkhEMGHsYeSyeU8qUzCDUXPjg==}
     hasBin: true
 
   '@microsoft/tsdoc-config@0.17.1':
@@ -1829,14 +1829,8 @@ packages:
   '@types/prompts@2.4.9':
     resolution: {integrity: sha512-qTxFi6Buiu8+50/+3DGIWLHM6QuWsEKugJnnP6iv2Mc4ncxE4A/OJkjuVOA+5X0X1S/nq5VJRa8Lu+nwcvbrKA==}
 
-  '@types/prop-types@15.7.14':
-    resolution: {integrity: sha512-gNMvNH49DJ7OJYv+KAKn0Xp45p8PLl6zo2YnvDIbTd4J6MER2BmWN49TG7n9LvkyihINxeKW8+3bfS2yDC9dzQ==}
-
-  '@types/react@18.3.18':
-    resolution: {integrity: sha512-t4yC+vtgnkYjNSKlFx1jkAhH8LgTo2N/7Qvi83kdEaUtMDiwpbLAktKDaAMlRcJ5eSxZkH74eEGt1ky31d7kfQ==}
-
-  '@types/react@19.0.3':
-    resolution: {integrity: sha512-UavfHguIjnnuq9O67uXfgy/h3SRJbidAYvNjLceB+2RIKVRBzVsh0QO+Pw6BCSQqFS9xwzKfwstXx0m6AbAREA==}
+  '@types/react@19.0.4':
+    resolution: {integrity: sha512-3O4QisJDYr1uTUMZHA2YswiQZRq+Pd8D+GdVFYikTutYsTz+QZgWkAPnP7rx9txoI6EXKcPiluMqWPFV3tT9Wg==}
 
   '@types/resolve@1.20.2':
     resolution: {integrity: sha512-60BCwRFOZCQhDncwQdxxeOEEkbc5dIMccYLwbxsS4TUNeVECQ/pBJ0j09mrHOl/JJvpRPGwO9SvE4nR2Nb/a4Q==}
@@ -5489,8 +5483,8 @@ packages:
     resolution: {integrity: sha512-3KS2b+kL7fsuk/eJZ7EQdnEmQoaho/r6KUef7hxvltNA5DR8NAUM+8wJMbJyZ4G9/7i3v5zPBIMN5aybAh2/Jg==}
     engines: {node: '>= 0.4'}
 
-  typescript@5.7.2:
-    resolution: {integrity: sha512-i5t66RHxDvVN40HfDd1PsEThGNnlMCMT3jMUuoh9/0TaqWevNontacunWyN02LA9/fIbEWlcHZcgTKb9QoaLfg==}
+  typescript@5.4.2:
+    resolution: {integrity: sha512-+2/g0Fds1ERlP6JsakQQDXjZdZMM+rqpamFZJEKh4kwTIn3iDkgKtby0CeNd5ATNZ4Ry1ax15TMx0W2V+miizQ==}
     engines: {node: '>=14.17'}
     hasBin: true
 
@@ -6662,7 +6656,7 @@ snapshots:
     transitivePeerDependencies:
       - '@types/node'
 
-  '@microsoft/api-extractor@7.49.0(@types/node@20.17.10)':
+  '@microsoft/api-extractor@7.48.1(@types/node@20.17.10)':
     dependencies:
       '@microsoft/api-extractor-model': 7.30.1(@types/node@20.17.10)
       '@microsoft/tsdoc': 0.15.1
@@ -6676,7 +6670,7 @@ snapshots:
       resolve: 1.22.9
       semver: 7.5.4
       source-map: 0.6.1
-      typescript: 5.7.2
+      typescript: 5.4.2
     transitivePeerDependencies:
       - '@types/node'
 
@@ -7197,14 +7191,7 @@ snapshots:
       '@types/node': 20.17.10
       kleur: 3.0.3
 
-  '@types/prop-types@15.7.14': {}
-
-  '@types/react@18.3.18':
-    dependencies:
-      '@types/prop-types': 15.7.14
-      csstype: 3.1.3
-
-  '@types/react@19.0.3':
+  '@types/react@19.0.4':
     dependencies:
       csstype: 3.1.3
 
@@ -11578,7 +11565,7 @@ snapshots:
       possible-typed-array-names: 1.0.0
       reflect.getprototypeof: 1.0.9
 
-  typescript@5.7.2: {}
+  typescript@5.4.2: {}
 
   typescript@5.7.3: {}
 

src/node/tasks/dts/createApiExtractorConfig.ts
@@ -26,24 +26,45 @@ export function createApiExtractorConfig(options: {
     tsconfigPath,
   } = options
 
+  // When the module is 'preserve' it's necessary to override the tsconfig file as api-extractor doesn't handle it out of the box
+  const workaroundModulePreserve = tsconfig.options.module === ts.ModuleKind.Preserve
+  // If `paths` are used for self-referencing imports (e.g. the module is named `sanity`, and the `sanity/structure` export is also importing from `sanity/router`),
+  const workaroundPaths = !!tsconfig.options.paths
+
+  const overrideTsconfig = {
+    extends: tsconfigPath,
+    compilerOptions: {},
+  }
+
+  if (workaroundModulePreserve) {
+    Object.assign(overrideTsconfig.compilerOptions, {
+      // Set the equivalent options to `module: 'Preserve'`
+      // https://github.com/microsoft/TypeScript/pull/56785/files?file-filters%5B%5D=.js&file-filters%5B%5D=.json&file-filters%5B%5D=.symbols&file-filters%5B%5D=.ts&file-filters%5B%5D=.types&show-viewed-files=true#diff-31d3c12bafea26bc9e8c8a77920c41af0c593206442add70c45a06c063767445
+      module: 'ESNext',
+      moduleResolution: 'Bundler',
+      esModuleInterop: true,
+      resolveJsonModule: true,
+    })
+  }
+
+  if (workaroundPaths) {
+    Object.assign(overrideTsconfig.compilerOptions, {
+      // An empty object replaces whatever is in the original tsconfig file
+      paths: {},
+    })
+  }
+
   return {
     apiReport: {
       enabled: false,
       reportFileName: '<unscopedPackageName>.api.md',
     },
     bundledPackages,
-    // If `paths` are used for self-referencing imports (e.g. the module is named `sanity`, and the `sanity/structure` export is also importing from `sanity/router`),
-    compiler: tsconfig.options.paths
-      ? {
-          overrideTsconfig: {
-            extends: tsconfigPath,
-            compilerOptions: {
-              // An empty object replaces whatever is in the original tsconfig file
-              paths: {},
-            },
-          },
-        }
-      : {tsconfigFilePath: tsconfigPath},
+
+    compiler:
+      workaroundModulePreserve || workaroundPaths
+        ? {overrideTsconfig}
+        : {tsconfigFilePath: tsconfigPath},
 
     docModel: {
       enabled: false,

test/__snapshots__/cli.test.ts.snap
@@ -227,21 +227,10 @@ export {
 `;
 
 exports[`should build \`multi-export\` package 6`] = `
-"/** @public */
-export declare function plugin(): Plugin_2
+"import {Plugin as Plugin_2} from 'multi-export'
 
 /** @public */
-declare interface Plugin_2 {
-  name: string
-  options?: PluginOptions
-}
-
-declare interface PluginOptions {
-  /**
-   * @defaultValue true
-   */
-  enabled?: boolean
-}
+export declare function plugin(): Plugin_2
 
 export {}
 "