e9c78c6509d518ab87ea1af621a2eef12a37aabd
Cody Olsen <stipsan@gmail.com>
2025-01-27 14:13:12 +0100

    chore: remove svelte tests

--- playground/multi-export/src/svelte/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './types'
-export {default as VisualEditing} from './VisualEditing.svelte'
--- playground/multi-export/src/svelte/types.ts
+++
@@ -1,4 +0,0 @@
-import type {VisualEditingOptions} from '@sanity/visual-editing'
-
-/** @public */
-export interface VisualEditingProps extends Pick<VisualEditingOptions, 'zIndex'> {}
