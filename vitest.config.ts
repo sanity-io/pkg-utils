917c09e87431cdf350be85180dc15b9ed988f761
Cody Olsen <stipsan@gmail.com>
2025-06-18 17:16:52 +0200

    chore: update vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -9,16 +9,16 @@ export default defineConfig({
     // Set to 2 minutes to support long-running Next.js test
     testTimeout: 2 * 60 * 1000,
 
-    // Enable rich PR failed test annotation on the CI
-    reporters: process.env.GITHUB_ACTIONS ? ['default', 'github-actions'] : 'default',
-
     // Reduce threads as suites will use the filesystem and `pnpm install` and need to run in sequence
     fileParallelism: false,
-
-    // Don't rerun test watcher when generated files change, or it'll infinitely loop
-    watchExclude: ['**/node_modules/**', '**/dist/**', 'test/env/__tmp__/**'],
   },
   esbuild: {
     target: 'node14',
+  },
+  server: {
+    watch: {
+      // Don't rerun test watcher when generated files change, or it'll infinitely loop
+      ignored: ['**/node_modules/**', '**/dist/**', 'test/env/__tmp__/**'],
+    },
   },
 })
