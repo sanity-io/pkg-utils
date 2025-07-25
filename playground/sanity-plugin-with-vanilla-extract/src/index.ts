8e8404faaba9ac4a4b9b127082e4feb3933660f0
Cody Olsen <stipsan@gmail.com>
2025-07-25 19:01:06 +0200

    chore: add test cases

---
+++ playground/sanity-plugin-with-vanilla-extract/src/index.ts
@@ -0,0 +1,14 @@
+import {definePlugin, type Plugin} from 'sanity'
+
+interface ColorOptions {
+  /**
+   * @defaultValue 'hex'
+   */
+  format?: 'hex' | 'rgb' | 'hsl' | 'hsv'
+  alpha?: boolean
+}
+
+/** @public */
+export const colorInput: Plugin<ColorOptions> = definePlugin<ColorOptions>({
+  name: '@sanity/color-input',
+})
