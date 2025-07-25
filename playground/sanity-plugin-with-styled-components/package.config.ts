ae646eaf943fb78687b29f4d014d3f2ffd2fc90d
Cody Olsen <stipsan@gmail.com>
2025-07-25 19:13:34 +0200

    chore: add 18 test case

--- playground/sanity-plugin-with-styled-components/package.config.ts
+++ playground/sanity-plugin-with-styled-components/package.config.ts
@@ -3,4 +3,11 @@ import {defineConfig} from '@sanity/pkg-utils'
 export default defineConfig({
   tsconfig: 'tsconfig.dist.json',
   dts: 'rolldown',
+  babel: {
+    reactCompiler: true,
+    styledComponents: true,
+  },
+  reactCompilerOptions: {
+    target: '18',
+  },
 })
