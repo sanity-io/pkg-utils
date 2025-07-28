8043ed60a11a9ce7a5d6378fe635d7b3878ce18b
Cody Olsen <stipsan@gmail.com>
2025-07-28 21:15:09 +0200

    chore: add vanilla-extract test

--- playground/sanity-plugin-with-vanilla-extract/package.config.ts
+++ playground/sanity-plugin-with-vanilla-extract/package.config.ts
@@ -1,8 +1,19 @@
 import {defineConfig} from '@sanity/pkg-utils'
+import {vanillaExtractPlugin} from '@vanilla-extract/rollup-plugin'
 
 export default defineConfig({
   tsconfig: 'tsconfig.dist.json',
   dts: 'rolldown',
   babel: {reactCompiler: true},
   reactCompilerOptions: {target: '19'},
+  rollup: {
+    output: {assetFileNames: '[name][extname]'},
+    plugins: [
+      vanillaExtractPlugin({
+        extract: {
+          name: 'style.css',
+        },
+      }),
+    ],
+  },
 })
