sanity-io/pkg-utils

commit 2c65e6375823325327bbf6a69ed712d12256246c

refactor!: resolve `package.config.ts` as ESM when supported (#2138)
BREAKING CHANGE:
This means that if the `package.json` has `"type": "commonjs"` then the `package.config.ts` can no longer use `import/export` keywords. If you're unable to start using `"type": "module"` you'll either have to rename to `package.config.mts`, or use `require/module.exports` instead.

src/node/core/config/loadConfig.ts
@@ -1,15 +1,8 @@
-import {createRequire} from 'node:module'
 import path from 'node:path'
 import {packageUp} from 'package-up'
-import {register} from 'tsx/cjs/api'
+import {tsImport} from 'tsx/esm/api'
 import {findConfigFile} from './findConfigFile.ts'
 import type {PkgConfigOptions} from './types.ts'
-
-const require = createRequire(import.meta.url)
-
-declare global {
-  var ___DEV___: boolean
-}
 
 /** @alpha */
 export async function loadConfig(options: {cwd: string}): Promise<PkgConfigOptions | undefined> {
@@ -32,11 +25,7 @@ export async function loadConfig(options: {cwd: string}): Promise<PkgConfigOptio
     return undefined
   }
 
-  const unregister = globalThis.___DEV___ ? () => undefined : register()
-
-  const mod = require(configFile)
-
-  unregister()
+  const mod = await tsImport(configFile, import.meta.url)
 
   return mod?.default || mod || undefined
 }

package.config.ts
@@ -1,5 +1,5 @@
+import {defineConfig} from '@sanity/pkg-utils'
 import {visualizer} from 'rollup-plugin-visualizer'
-import {defineConfig} from './src/node/index.ts'
 
 export default defineConfig({
   bundles: [

playground/sanity/package.config.mts
@@ -1,5 +1,5 @@
 import {defineConfig} from '@sanity/pkg-utils'
-import baseConfig from './repo.package.config'
+import baseConfig from './repo.package.config.mts'
 
 export default defineConfig({
   ...baseConfig,

playground/vision/package.config.mts
@@ -1,4 +1,4 @@
 import {defineConfig} from '@sanity/pkg-utils'
-import baseConfig from './repo.package.config'
+import baseConfig from './repo.package.config.mts'
 
 export default defineConfig({...baseConfig, external: ['sanity']})

scripts/build.ts
@@ -4,8 +4,6 @@ import {build} from '../src/node/index.ts'
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
-global.___DEV___ = true
-
 build({cwd: path.resolve(__dirname, '..'), strict: true}).catch((err) => {
   console.error(err.stack)
   process.exit(1)

commit 9560fb5a186c4a473e2cab42516c0ef7dcc69965

fix: regression in #2138

src/node/core/config/loadConfig.ts
@@ -1,4 +1,5 @@
 import path from 'node:path'
+import {pathToFileURL} from 'node:url'
 import {packageUp} from 'package-up'
 import {tsImport} from 'tsx/esm/api'
 import {findConfigFile} from './findConfigFile.ts'
@@ -25,7 +26,7 @@ export async function loadConfig(options: {cwd: string}): Promise<PkgConfigOptio
     return undefined
   }
 
-  const mod = await tsImport(configFile, import.meta.url)
+  const mod = await tsImport(pathToFileURL(configFile).toString(), import.meta.url)
 
   return mod?.default || mod || undefined
 }