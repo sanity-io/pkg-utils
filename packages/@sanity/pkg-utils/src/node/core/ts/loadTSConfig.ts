sanity-io/pkg-utils

commit 35db6284f7554a7b68e987a63a9cda9e90dbfa1b

Enable type-aware linting with oxlint (#2175)

packages/@sanity/pkg-utils/src/node/core/ts/loadTSConfig.ts
@@ -7,13 +7,13 @@ export async function loadTSConfig(options: {
 }): Promise<ts.ParsedCommandLine | undefined> {
   const {cwd, tsconfigPath} = options
 
-  const configPath = ts.findConfigFile(cwd, ts.sys.fileExists, tsconfigPath)
+  const configPath = ts.findConfigFile(cwd, (fileName) => ts.sys.fileExists(fileName), tsconfigPath)
 
   if (!configPath) {
     return undefined
   }
 
-  const configFile = ts.readConfigFile(configPath, ts.sys.readFile)
+  const configFile = ts.readConfigFile(configPath, (path) => ts.sys.readFile(path))
 
   return ts.parseJsonConfigFileContent(configFile.config, ts.sys, cwd)
 }

packages/@sanity/pkg-utils/src/node/tasks/dts/dtsWatchTask.ts
@@ -78,6 +78,9 @@ export const dtsWatchTask: TaskHandler<DtsWatchTask, DtsResult> = {
       )
 
       const origPostProgramCreate = host.afterProgramCreate
+        ? (program: ts.EmitAndSemanticDiagnosticsBuilderProgram) =>
+            host.afterProgramCreate?.(program)
+        : undefined
 
       host.afterProgramCreate = async (program) => {
         origPostProgramCreate?.(program)

packages/@sanity/pkg-utils/src/node/build.ts
@@ -74,6 +74,7 @@ export async function build(options: {
   const buildTasks = resolveBuildTasks(ctx)
 
   for (const task of buildTasks) {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-type-assertion -- TypeScript can't infer the correct handler type from discriminated union
     const handler = buildTaskHandlers[task.type] as TaskHandler<BuildTask>
     const taskName = handler.name(ctx, task)
 
@@ -90,7 +91,7 @@ export async function build(options: {
       spinner.error()
 
       if (err instanceof Error) {
-        const RE_CWD = new RegExp(`${cwd}`, 'g')
+        const RE_CWD = new RegExp(cwd, 'g')
 
         ctx.logger.error(err.message.replace(RE_CWD, '.'))
         ctx.logger.log()

packages/@sanity/pkg-utils/src/node/check.ts
@@ -74,13 +74,16 @@ export async function check(options: {
 
       const consoleSpy = createConsoleSpy()
 
+      const checks = []
       if (exportPaths.import.length) {
-        checkExports(exportPaths.import, {cwd, external, format: 'esm', logger})
+        checks.push(checkExports(exportPaths.import, {cwd, external, format: 'esm', logger}))
       }
 
       if (exportPaths.require.length) {
-        checkExports(exportPaths.require, {cwd, external, format: 'cjs', logger})
+        checks.push(checkExports(exportPaths.require, {cwd, external, format: 'cjs', logger}))
       }
+
+      await Promise.all(checks)
 
       consoleSpy.restore()
     }
@@ -94,7 +97,7 @@ export async function check(options: {
     spinner.error()
 
     if (err instanceof Error) {
-      const RE_CWD = new RegExp(`${cwd}`, 'g')
+      const RE_CWD = new RegExp(cwd, 'g')
 
       logger.error((err.stack || err.message).replace(RE_CWD, '.'))
       logger.log()
@@ -166,7 +169,7 @@ async function checkExports(
 
       logger.log()
     } else {
-      logger.error(`${err}`)
+      logger.error(String(err))
 
       logger.log()
     }

packages/@sanity/pkg-utils/src/node/core/config/loadConfig.ts
@@ -15,7 +15,7 @@ export async function loadConfig(options: {cwd: string}): Promise<PkgConfigOptio
 
   const root = path.dirname(pkgPath)
 
-  const configFile = await findConfigFile(root)
+  const configFile = findConfigFile(root)
 
   if (!configFile) {
     return undefined

packages/@sanity/pkg-utils/src/node/core/config/resolveConfigProperty.ts
@@ -1,5 +1,11 @@
 import type {PkgConfigProperty, PkgConfigPropertyResolver} from './types.ts'
 
+function isPkgConfigPropertyResolver<T>(
+  prop: PkgConfigProperty<T>,
+): prop is PkgConfigPropertyResolver<T> {
+  return typeof prop === 'function'
+}
+
 /** @internal */
 export function resolveConfigProperty<T>(
   prop: PkgConfigProperty<T> | undefined,
@@ -7,8 +13,8 @@ export function resolveConfigProperty<T>(
 ): T {
   if (!prop) return initialValue
 
-  if (typeof prop === 'function') {
-    return (prop as PkgConfigPropertyResolver<T>)(initialValue)
+  if (isPkgConfigPropertyResolver(prop)) {
+    return prop(initialValue)
   }
 
   return prop

packages/@sanity/pkg-utils/src/node/core/pkg/parseExports.ts
@@ -9,6 +9,11 @@ import {pkgExtMap} from './pkgExt.ts'
 import type {PackageJSON} from './types.ts'
 import {validateExports} from './validateExports.ts'
 
+// Type guard to filter out falsy values
+function isTruthy<T>(value: T | false | null | undefined | 0 | ''): value is T {
+  return Boolean(value)
+}
+
 /** @alpha */
 export function parseExports(options: {
   cwd: string
@@ -89,7 +94,7 @@ export function parseExports(options: {
       }
 
       errors.push(
-        ...([
+        ...[
           'package.json: `exports` are missing, it should be:',
           `"exports": {`,
           `  ".": {`,
@@ -102,7 +107,7 @@ export function parseExports(options: {
           `  },`,
           `  "./package.json": "./package.json"`,
           `}`,
-        ].filter(Boolean) as string[]),
+        ].filter(isTruthy),
       )
     }
   }

packages/@sanity/pkg-utils/src/node/core/pkg/validatePkg.ts
@@ -18,8 +18,10 @@ const pkgSchema = z.object({
   exports: z.optional(
     z.record(
       z.union([
-        z.custom<`./${string}.json`>((val) => /^\.\/.*\.json$/.test(val as string)),
-        z.custom<`./${string}.css`>((val) => /^\.\/.*\.css$/.test(val as string)),
+        z.custom<`./${string}.json`>(
+          (val) => typeof val === 'string' && /^\.\/.*\.json$/.test(val),
+        ),
+        z.custom<`./${string}.css`>((val) => typeof val === 'string' && /^\.\/.*\.css$/.test(val)),
         z.object({
           types: z.optional(z.string()),
           source: z.optional(z.string()),
@@ -65,6 +67,7 @@ for (const key of pkgSchema.keyof()._def.values) {
 export function validatePkg(input: unknown): PackageJSON {
   const pkg = pkgSchema.parse(input)
 
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-type-assertion -- Need to check raw input for typos
   const invalidKey = Object.keys(input as PackageJSON).find((key) => {
     const needle = key.toUpperCase()
 

commit 73ed592e24e4552bac2037aa660a2aee742f245c

fix: call stack size exceeded regression
Fixes a regression introduced by #2175 leading to a max call stack size exceeded error when running `pkg-utils watch`

packages/@sanity/pkg-utils/src/node/core/ts/loadTSConfig.ts
@@ -7,13 +7,15 @@ export async function loadTSConfig(options: {
 }): Promise<ts.ParsedCommandLine | undefined> {
   const {cwd, tsconfigPath} = options
 
-  const configPath = ts.findConfigFile(cwd, (fileName) => ts.sys.fileExists(fileName), tsconfigPath)
+  // oxlint-disable-next-line unbound-method
+  const configPath = ts.findConfigFile(cwd, ts.sys.fileExists, tsconfigPath)
 
   if (!configPath) {
     return undefined
   }
 
-  const configFile = ts.readConfigFile(configPath, (path) => ts.sys.readFile(path))
+  // oxlint-disable-next-line unbound-method
+  const configFile = ts.readConfigFile(configPath, ts.sys.readFile)
 
   return ts.parseJsonConfigFileContent(configFile.config, ts.sys, cwd)
 }

packages/@sanity/pkg-utils/src/node/tasks/dts/dtsWatchTask.ts
@@ -77,10 +77,8 @@ export const dtsWatchTask: TaskHandler<DtsWatchTask, DtsResult> = {
         },
       )
 
+      // oxlint-disable-next-line unbound-method
       const origPostProgramCreate = host.afterProgramCreate
-        ? (program: ts.EmitAndSemanticDiagnosticsBuilderProgram) =>
-            host.afterProgramCreate?.(program)
-        : undefined
 
       host.afterProgramCreate = async (program) => {
         origPostProgramCreate?.(program)