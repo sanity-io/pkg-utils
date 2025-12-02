b283bb790594bee79c5d395dca6503980b650e3b
Cody Olsen <stipsan@gmail.com>
2025-12-03 00:12:04 +0100

    Update globalSetup.ts

--- packages/@sanity/tsdown-config/test/globalSetup.ts
+++ packages/@sanity/tsdown-config/test/globalSetup.ts
@@ -3,7 +3,7 @@ import {x} from 'tinyexec'
 export async function setup() {
   const controller = new AbortController()
   const {signal} = controller
-  await x('pnpm', ['--filter', '@fixtures/*','run', 'build'], {throwOnError: true,
+  await x('pnpm', ['--filter', '"./packages/@sanity/tsdown-config/test/fixtures/**"', '--parallel', '--stream', '-r', 'run', 'build'], {throwOnError: true,
     signal,
   })
 
