6200ce0ee6ab61be5a7e6280e8639949b12c988b
Cody Olsen <stipsan@gmail.com>
2025-12-02 23:46:09 +0100

    fix build

--- packages/@sanity/tsdown-config/test/globalSetup.ts
+++ packages/@sanity/tsdown-config/test/globalSetup.ts
@@ -3,7 +3,6 @@ import {x} from 'tinyexec'
 export async function setup() {
   const controller = new AbortController()
   const {signal} = controller
-  await x('pnpm', ['--filter', "@sanity/tsdoc-config", 'run', 'build'], {throwOnError: true, signal})
   await x(
     'pnpm',
     ['--filter', './packages/@sanity/tsdown-config/test/fixtures/**', 'run', 'build'],
