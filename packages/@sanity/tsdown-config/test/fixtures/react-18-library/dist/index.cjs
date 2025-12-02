59271507641ab449772da2f6c07adec1df46ef8e
Cody Olsen <stipsan@gmail.com>
2025-12-02 22:59:26 +0100

    add more snapshots

---
+++ packages/@sanity/tsdown-config/test/fixtures/react-18-library/dist/index.cjs
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: !0 });
+var jsxRuntime = require("react/jsx-runtime");
+function Button({ children, type = "button" }) {
+  return /* @__PURE__ */ jsxRuntime.jsx("button", { type, children });
+}
+exports.Button = Button;
+//# sourceMappingURL=index.cjs.map
