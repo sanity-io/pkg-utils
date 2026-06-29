7a43ea46a60403b6cd5af4247b939a58fcc8937c
filmaj <fil.maj@sanity.io>
2026-06-29 15:08:44 -0400

    fix: make order of paths passed into `findCommonDirPath` irrelevant

--- packages/@sanity/pkg-utils/src/node/core/findCommonPath.test.ts
+++ packages/@sanity/pkg-utils/src/node/core/findCommonPath.test.ts
@@ -3,6 +3,7 @@ import {findCommonDirPath} from './findCommonPath.ts'
 
 test('should find common parent directory of paths', () => {
   expect(findCommonDirPath(['test/a', 'test/b/a'])).toBe('test')
+  expect(findCommonDirPath(['test/a/b/c', 'test/a/b'])).toBe('test/a')
   expect(findCommonDirPath(['/test/a', '/test/b/a'])).toBe('/test')
   expect(findCommonDirPath(['/test/a/b', '/test/a/b/c'])).toBe('/test/a')
   expect(findCommonDirPath(['/test/a/b/c', '/test/a/b'])).toBe('/test/a')
