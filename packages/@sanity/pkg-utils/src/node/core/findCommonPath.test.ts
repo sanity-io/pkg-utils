74a9947ce55595c0974d838e7097830b73229820
filmaj <fil.maj@sanity.io>
2026-06-29 14:43:59 -0400

    test(chore): expand findCommonPath test cases to find a failing case

--- packages/@sanity/pkg-utils/src/node/core/findCommonPath.test.ts
+++ packages/@sanity/pkg-utils/src/node/core/findCommonPath.test.ts
@@ -5,5 +5,6 @@ test('should find common parent directory of paths', () => {
   expect(findCommonDirPath(['test/a', 'test/b/a'])).toBe('test')
   expect(findCommonDirPath(['/test/a', '/test/b/a'])).toBe('/test')
   expect(findCommonDirPath(['/test/a/b', '/test/a/b/c'])).toBe('/test/a')
+  expect(findCommonDirPath(['/test/a/b/c', '/test/a/b'])).toBe('/test/a')
   expect(findCommonDirPath(['test/a/b', 'test/a/b', 'test/b/c', 'test/b/a'])).toBe('test')
 })
